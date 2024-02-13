380e2f7d8286002f39b7997181926e38ffed89df
Mattia <mattia@MBP-di-Mattia.station>
2024-02-13 13:09:47 +0100

    add ejs

---
+++ 1.5 express - ejs/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    const d = new Date(); // get current date
+    const day = d.getDay(); // get current day number from current date
+
+    let type = "it's a workday";
+    let adv = "let's work";
+
+    if (day === 6 || day === 0) {
+        type = "it's a weekend";
+        adv = "have a nice weekend";
+    }
+
+    res.render(`index.ejs`, {
+        type: type,
+        advice: adv
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`);
+});
